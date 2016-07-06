6c373bbda5dea27dc430d4a37aa7f06801bc7cf3
Erin Kelkar <ekelkar@gmail.com>
2016-07-05 21:55:17 -0400

    Simple language evaluation working.

--- chapter11-ProgramLang/programLang.js
+++ chapter11-ProgramLang/programLang.js
@@ -101,6 +101,107 @@ function parse(program) {
     return result.expr;
 }
 
+var specialForms = Object.create(null);
+
+function evaluate(expr, env) {
+    var op;
+    switch (expr.type) {
+
+    case "value":
+        // Return the value of string and numeric literals.   
+        return expr.value;
+
+    case "word":
+        // Look up variables in the global environment
+        // Replaced the following with the above due to jslint warnings.
+        //if (env.hasOwnProperty(expr.name)) {
+        if (expr.name in env) {
+            return env[expr.name];
+        } // else {
+        throw new ReferenceError("Undefined variable: " + expr.name);
+        //    }
+
+    case "apply":
+        if (expr.operator.type === "word" &&
+            // specialForms.hasOwnProperty(expr.operator.name)) {
+            // Replaced the following with above due to jslint warnings.
+            expr.operator.name in specialForms) {
+            return specialForms[expr.operator.name](expr.args, env);
+        }
+        op = evaluate(expr.operator, env);
+        if (typeof op !== "function") {
+            throw new TypeError("Applying a non-function.");
+        }
+        return op.apply(null, expr.args.map(function (arg) {
+            return evaluate(arg, env);
+        }));
+    }
+}
+
+specialForms["if"] = function (args, env) {
+    if (args.length !== 3) {
+        throw new SyntaxError("Bad number of args to if");
+    }
+    if (evaluate(args[0], env) !== false) {
+        return evaluate(args[1], env);
+    } // Changed because of jslint hint else {
+    return evaluate(args[2], env);
+    //  }
+};
+
+specialForms["while"] = function (args, env) {
+    if (args.length !== 2) {
+        throw new SyntaxError("Bad number of Args to while");
+    }
+    while (evaluate(args[0], env) !== false) {
+        evaluate(args[1], env);
+
+        // Since undefined does not exist in Egg, we return false,
+        // for lack of a meaningful result.
+    }
+};
+
+specialForms["do"] = function (args, env) {
+    var value = false;
+
+    args.forEach(function (arg) {
+        value = evaluate(arg, env);
+    });
+    return value;
+};
+
+specialForms["define"] = function (args, env) {
+    var value;
+
+    if (args.length !== 2 || args[0].type !== "word") {
+        throw new SyntaxError("Bad use of define");
+    }
+    value = evaluate(args[1], env);
+    env[args[0].name] = value;
+    return value;
+};
+
+var topEnv = Object.create(null);
+
+topEnv["true"] = true;
+topEnv["false"] = false;
+
+["+", "-", "*", "/", "==", "<", ">"].forEach(function (op) {
+    topEnv[op] = new Function("a, b", "return a " + op + " b;");
+});
+
+topEnv["print"] = function (value) {
+    console.log(value);
+    return value;
+};
+
+function run() {
+    var env = Object.create(topEnv);
+    var program = Array.prototype.slice.call(arguments, 0).join("\n");
+    return evaluate(parse(program), env);
+}
+
+
 //console.log("short expression: ", parseExpression('"short"'));
 //console.log("long expression: ", parseExpression('"long expression"'));
 //console.log("short number: ", parseExpression('5'));
@@ -108,5 +209,19 @@ function parse(program) {
 //console.log("word: ", parseExpression('a'));
 //console.log("word: ", parseExpression('='));
 //console.log("word: ", parseExpression('a+b'));
-console.log(parse("+(a, 10)"));
-console.log("Invalid: ", parseExpression("'a+b'")); // strings cannot have +
+//console.log(parse("+(a, 10)"));
+//console.log("Invalid: ", parseExpression("'a+b'")); // strings cannot have +
+
+//run("do(define(a, 10),",
+//    "  print(+(a, 10)))");
+
+run("do(define(total, 15),",
+    //    "   define(count, 1),",
+    //    "   while(<(count, 11),",
+    //    "         do(define(total, +(total, count)),",
+    //    "            define(count, +(count, 1)))),",
+    "   print(total))");
+
+
+console.log(parse(prog));
+console.log(evaluate(prog, topEnv));
