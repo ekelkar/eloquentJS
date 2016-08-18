82baeafcaa37d1281da39601ce697dcac75a980f
Erin Kelkar <ekelkar@gmail.com>
2016-08-18 12:47:01 -0400

    Clear and redraw board on window resize, refactored code.

--- chapter14-HandlingEvents/lightUp.js
+++ chapter14-HandlingEvents/lightUp.js
@@ -12,75 +12,78 @@ var colors = [GREEN, BLUE, YELLOW, PINK, DEFAULT];
 var BOXSIZE = 16; // size of each box in px
 var MARGIN = 8; // size of margin around body in px 
 
-var noOfColumns = Math.floor((window.innerWidth - MARGIN * 2) / BOXSIZE);
-var noOfRows = Math.floor((window.innerHeight - MARGIN * 2) / BOXSIZE);
-var row, column = 0; // loop indexes
-
-var boxdiv;
-var halfboxdiv;
-var body;
-var dotdiv;
-var cleardiv;
-var dotdivs;
-var elem;
-var colorIndex;
-var currentId = 0;
 var board;
 
+function createBoard() {
+    var noOfColumns = Math.floor((window.innerWidth - MARGIN * 2) / BOXSIZE);
+    var noOfRows = Math.floor((window.innerHeight - MARGIN * 2) / BOXSIZE);
+    var row, column = 0; // loop indexes
+    var cleardiv;
+    var currentId = 0;
+    var body;
 
-function addOddRow() {
-    // add first half-box
-    halfboxdiv = document.createElement('div');
-    halfboxdiv.className = 'half-box';
-    document.body.appendChild(halfboxdiv);
+    function createHalfBox() {
+        var halfboxdiv;
 
-    for (column = 0; column < noOfColumns - 1; column += 1) {
+        halfboxdiv = document.createElement('div');
+        halfboxdiv.className = 'half-box';
+
+        return halfboxdiv;
+    }
+
+    function createBoxWithDot() {
+        var boxdiv;
+        var dotdiv;
+
         boxdiv = document.createElement('div');
         boxdiv.className = 'box';
         dotdiv = document.createElement('div');
         dotdiv.className = 'dot';
         dotdiv.id = 'dot' + currentId.toString();
         currentId += 1;
-        // dotdiv.id = 'dot' + (row * SIZE + column).toString();
         boxdiv.appendChild(dotdiv);
-        document.body.appendChild(boxdiv);
-    }
-    // add last half-box
-    halfboxdiv = document.createElement('div');
-    halfboxdiv.className = 'half-box';
-    document.body.appendChild(halfboxdiv);
-}
 
-function addEvenRow() {
-    for (column = 0; column < noOfColumns; column += 1) {
-        boxdiv = document.createElement('div');
-        boxdiv.className = 'box';
-        dotdiv = document.createElement('div');
-        dotdiv.className = 'dot';
-        dotdiv.id = 'dot' + currentId.toString();
-        currentId += 1;
-        // dotdiv.id = 'dot' + (row * SIZE + column).toString();
-        boxdiv.appendChild(dotdiv);
-        document.body.appendChild(boxdiv);
-    }
-}
-
-body = document.querySelector('body');
-for (row = 0; row < noOfRows; row += 1) {
-    if (row % 2 === 0) {
-        addEvenRow();
-    } else {
-        addOddRow();
+        return boxdiv;
     }
 
-    // clear the left float
-    cleardiv = document.createElement('div');
-    cleardiv.className = 'left-clear';
-    document.body.appendChild(cleardiv);
+    function addOddRow() {
+        // add first half-box
+        document.body.appendChild(createHalfBox());
+
+        for (column = 0; column < noOfColumns - 1; column += 1) {
+            document.body.appendChild(createBoxWithDot());
+        }
+
+        // add last half-box
+        document.body.appendChild(createHalfBox());
+    }
+
+    function addEvenRow() {
+        for (column = 0; column < noOfColumns; column += 1) {
+            document.body.appendChild(createBoxWithDot());
+        }
+    }
+
+    body = document.querySelector('body');
+    for (row = 0; row < noOfRows; row += 1) {
+        if (row % 2 === 0) {
+            addEvenRow();
+        } else {
+            addOddRow();
+        }
+
+        // clear the left float
+        cleardiv = document.createElement('div');
+        cleardiv.className = 'left-clear';
+        document.body.appendChild(cleardiv);
+    }
 }
 
 function changeDotColor(e) {
     var clickedItem;
+    var elem;
+    var colorIndex;
+
     if (e.target !== e.currentTarget) {
         clickedItem = e.target.id;
         // Only dots have an id so only continue if there is an id (i.e., dot clicked)
@@ -97,7 +100,24 @@ function changeDotColor(e) {
     e.stopPropagation();
 }
 
+function redrawBoard() {
+    var board = document.querySelector('#board');
+    while (board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
+    createBoard();
+}
+var timeout;
+
+function resizeTimeout() {
+    console.log('window resized');
+    clearTimeout(timeout);
+    timeout = setTimeout(redrawBoard, 1000);
+}
+
+createBoard();
 // Changed from event listener for each do to one for the board.
 // See www.kirupa.com/html5/handling_events_for_many_elements.htm
 board = document.querySelector('#board');
 board.addEventListener('click', changeDotColor, false);
+addEventListener('resize', resizeTimeout);
